11a647d1a67968d68917a43e4fc080a640c03629
gowtham-ggg <gowthamvinoth2015@gmail.com>
2025-03-30 12:02:45 +0530

    fixed error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatapp-fullstack-1pgb.onrender.com/api",
   withCredentials: true,
 });
